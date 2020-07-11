796685c7a52e3d9185c687350d94da02b1919743
immersvin <immersv@gmail.com>
2020-07-11 16:29:43 +0530

    List Animation from url

---
+++ Assets/AnimationPOc/AnimationPOCFromUrl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TriLib;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class AnimationPOCFromUrl : MonoBehaviour
+{
+    // this script allow users to download animation in 3d model from url
+
+    public string url,extension;
+    
+
+    private void Awake()
+    {
+        gameObject.AddComponent<AssetDownloader>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        var assetDownloader = GetComponent<AssetDownloader>();
+        assetDownloader.DownloadAsset(url, extension,null,null,null,null,null);
+        
+    }
+   
+}
