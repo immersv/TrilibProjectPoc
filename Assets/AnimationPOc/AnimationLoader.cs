b8610b6c183886005db2e5efdaad13746465ae96
immersvin <immersv@gmail.com>
2020-07-11 13:48:15 +0530

    no message

--- Assets/AnimationPOc/AnimationLoader.cs
+++ Assets/AnimationPOc/AnimationLoader.cs
@@ -7,8 +7,8 @@ public class AnimationLoader : MonoBehaviour
 {
     //this Script is to load animations from local Resource folder
     // Start is called before the first frame update
-   
-    
+
+    Animation anim;
     private void Awake()
     {
       
@@ -36,13 +36,38 @@ public class AnimationLoader : MonoBehaviour
             Debug.LogError("Error during import of models: ModelImporter is null");
             return;
         }*/
+
         ModelImporter MI = (ModelImporter)AssetImporter.GetAtPath("Assets/Spider.fbx");
         var skinObj = AssetDatabase.LoadAssetAtPath<Object>(MI.assetPath);
         var prefabInstance = (GameObject)GameObject.Instantiate(skinObj);
+        if (prefabInstance == null)
+        {
+            Debug.LogError("Failed to initialize prefab " + skinObj);
+            return;
+        }
+        else
+        {
+            //anim= prefabInstance.AddComponent<Animation>();
+        }
         ModelImporterClipAnimation[] clips = MI.defaultClipAnimations;
-        foreach (var c in clips)
+        foreach (ModelImporterClipAnimation c in clips)
         {
             Debug.Log(c.name);
+            c.loopTime = true;
+                      
+        }
+        MI.clipAnimations = clips;
+        AssetDatabase.ImportAsset(MI.assetPath);
+
+        // or get animation Clip
+        Object[] objects = AssetDatabase.LoadAllAssetsAtPath(MI.assetPath);
+        foreach (Object obj in objects)
+        {
+            AnimationClip clip = obj as AnimationClip;
+            if (clip != null)
+            {
+                anim.AddClip(clip,obj.name);
+            }
         }
     }
 }
