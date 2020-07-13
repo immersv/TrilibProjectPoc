eb4902b348d6865d76edb9986a8477d16192fe4b
immersvin <immersv@gmail.com>
2020-07-14 04:51:17 +0530

    Got the List of All GameObjects in Template

--- Assets/AllComponentsPOC/GetAssetBundle.cs
+++ Assets/AllComponentsPOC/GetAssetBundle.cs
@@ -22,8 +22,10 @@ public class GetAssetBundle : MonoBehaviour
         }
     }
     //  public Transform[] allgameObjects;
-    public List<GameObject> allgameObjects;
-   // public List<GameObject> allchildgameObjects;
+    //public List<GameObject> allgameObjects;
+    // public List<GameObject> allchildgameObjects;
+
+    public List<Transform> childs;
     IEnumerator GetAssetbundle(string uri)
     {
         UnityWebRequest www = UnityWebRequestAssetBundle.GetAssetBundle(uri);
@@ -38,37 +40,54 @@ public class GetAssetBundle : MonoBehaviour
             bundle = DownloadHandlerAssetBundle.GetContent(www);
             Debug.Log("Sucessfull");
             GameObject gm=Instantiate(bundle.LoadAsset("xlzbctemplate1")) as GameObject;
-           
-            foreach(Transform c in gm.transform)
-            {
-                if (c != null)
-                {
-                    allgameObjects.Add(c.gameObject);
-                    Debug.Log("All GameObjectNames " + c.gameObject.name);
-                    if (c.gameObject != null)
-                    {
-                        foreach (Transform c1 in c.transform)
-                        {
-                            allgameObjects.Add(c1.gameObject);
-                            Debug.Log("All Child GameObjectNames " + c1.gameObject.name);
-                            if (c1.gameObject != null)
-                             {
-                                 foreach(Transform c2 in c1.transform)
-                                 {
-                                     allgameObjects.Add(c2.gameObject);
-                                     Debug.Log("All Child in child GameObjectNames " + c2.gameObject.name);
-                                 }
-                             }
-                        }
+             childs = TransformExtension.GetAllChildren(gm.transform,null);
 
-                    }
-                }
+            /* foreach(Transform c in gm.transform)
+             {
+                 if (c != null)
+                 {
+                     allgameObjects.Add(c.gameObject);
+                     Debug.Log("All GameObjectNames " + c.gameObject.name);
+
+                     /*if (c.gameObject != null)
+                     {
+                         foreach (Transform c1 in c.transform)
+                         {
+                             allgameObjects.Add(c1.gameObject);
+                             Debug.Log("All Child GameObjectNames " + c1.gameObject.name);
+                             if (c1.gameObject != null)
+                              {
+                                  foreach(Transform c2 in c1.transform)
+                                  {
+                                      allgameObjects.Add(c2.gameObject);
+                                      Debug.Log("All Child in child GameObjectNames " + c2.gameObject.name);
+                                  }
+                              }
+                         }*/
+
+        }
+                
                 
                 
                  
                 
-            }
+            
            
+        
+    }
+}
+public static class TransformExtension
+{
+    public static List<Transform> GetAllChildren(this Transform parent, List<Transform> transformList = null)
+    {
+        if (transformList == null) transformList = new List<Transform>();
+
+        foreach (Transform child in parent)
+        {
+            transformList.Add(child);
+            child.GetAllChildren(transformList);
+            Debug.Log("Game object Name: "+child.name);
         }
+        return transformList;
     }
 }
